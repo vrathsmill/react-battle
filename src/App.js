1955386014ee87019d364aafdb9d493ecf67d28b
vrathsmill <vlrathsmill@gmail.com>
2017-08-16 17:06:03 -0400

    styling and axios and more

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import Popular from './components/Popular'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hi, {this.props.name}
+      <div className="container">
+      <Popular />
       </div>
     );
   }
